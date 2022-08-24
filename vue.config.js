f49a8b28c532052954b3b5c890490fdae313b60c
EighteenLao <e80677@yahoo.com.tw>
2022-08-24 10:42:05 +0800

    vue.config 404 delete HelloWorld

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   //transpileDependencies: true
   publicPath: process.env.NODE_ENV === 'production'
-      ? '/myRoomRentWebVue/' // test20200915 為 repo 名稱
+      ? '/myRoomRentWebVue/' // myRoomRentWebVue 為 repo 名稱
       : '/'
 })
